47edaeddf327af3e54a0b3632b89ac6ae87131ba
Josh Fisher <jfisherd@gmail.com>
2025-11-07 12:45:29 -0500

    console.log()'s added, logged objects unexpectedly empty

--- script.js
+++ script.js
@@ -11,16 +11,21 @@ blogs = document.getElementById("blogs")
 submit.addEventListener('click', (event) => {
     event.preventDefault()
     postContainer = document.createElement("li")
+    console.log('postContainer'+JSON.stringify(postContainer))
     postTitle = document.createElement("h3")
     postTitle.textContent = title.value
+    console.log('postTitle'+JSON.stringify(postTitle))
     postContent = document.createElement("div")
     postContent.textContent = content.value
+    console.log('postContent'+JSON.stringify(postContent))
     postContainer.append(postTitle, postContent)
+    console.log('postContainer'+JSON.stringify(postContainer))
     myPosts[myPosts.length] = postContainer
+    console.log('myPosts'+JSON.stringify(myPosts))
     blogs.append(postContainer)
-    title.value = ''
-    content.value = ''
-
+    console.log('blogs'+JSON.stringify(blogs))
+    // title.value = ''
+    // content.value = ''
     localStorage.setItem('myPosts', JSON.stringify(myPosts))
 })
 
